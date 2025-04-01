1cb4037c3a4a9f5f86b76ecf4781d7fd44d441d1
ricainigoikang14 <inigorica5@gmail.com>
2025-04-01 21:40:30 +0800

    Create ArrayActivity.js

---
+++ ArrayActivity.js
@@ -0,0 +1,12 @@
+const favSingers = ["lalisa", "Jennie", "Rose"]; 
+console.log(favSingers[0]); 
+
+const favNumbers = [7, 18, 23, 40];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]); 
+console.log(mixedArr[1][0]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]);
